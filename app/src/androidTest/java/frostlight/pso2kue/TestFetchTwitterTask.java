f2937012dbfb59c9b1d5e49bd93d9b0b5b75a8be
Vincent <cloudy.sonata@gmail.com>
2015-05-19 19:42:02 -0400

    Extend AndroidTestCase on TestFetchTwitterTask

--- app/src/androidTest/java/frostlight/pso2kue/TestFetchTwitterTask.java
+++ app/src/androidTest/java/frostlight/pso2kue/TestFetchTwitterTask.java
@@ -1,10 +1,12 @@
 package frostlight.pso2kue;
 
+import android.test.AndroidTestCase;
+
 /**
  * TestFetchTwitterTask
  * Test cases for Twitter fetching
  * Created by Vincent on 5/19/2015.
  */
-public class TestFetchTwitterTask {
+public class TestFetchTwitterTask extends AndroidTestCase {
 
 }
