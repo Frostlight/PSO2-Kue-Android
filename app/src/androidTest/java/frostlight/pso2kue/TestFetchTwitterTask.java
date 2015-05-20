cc9d76c1736048effb7448fe250b30d62d77b77e
Vincent <cloudy.sonata@gmail.com>
2015-05-20 05:29:47 -0400

    It is not simple! Also, typos

--- app/src/androidTest/java/frostlight/pso2kue/TestFetchTwitterTask.java
+++ app/src/androidTest/java/frostlight/pso2kue/TestFetchTwitterTask.java
@@ -8,14 +8,14 @@ import android.test.AndroidTestCase;
  * Created by Vincent on 5/19/2015.
  */
 public class TestFetchTwitterTask extends AndroidTestCase {
-    //Test key values are taken from Twitter's authentication references
-    //Source: https://dev.twitter.com/oauth/application-only
+    // Test key values are taken from Twitter's authentication references
+    // Source: https://dev.twitter.com/oauth/application-only
     public static String CONSUMER_KEY_TEST = "xvz1evFS4wEEPTGEFPHBog";
     public static String CONSUMER_SECRET_TEST = "TDhxcTlQWnlSZzZpZUtHRUtoWm9sR0MwdkpXTHc4aUVKODhEUmR5T2c=";
     public static String BARRIER_64_TOKEN =
             "eHZ6MWV2RlM0d0VFUFRHRUZQSEJvZzpMOHFxOVBaeVJnNmllS0dFS2hab2xHQzB2SldMdzhpRUo4OERSZHlPZw==";
 
-    //Tests if the consumer key and secret are encoded properly
+    // Tests if the consumer key and secret are encoded properly
     public void testEncodeKey() {
         String decodedString = FetchTwitterTask.encodeKey(CONSUMER_KEY_TEST, CONSUMER_SECRET_TEST);
         assertEquals(BARRIER_64_TOKEN, decodedString);
