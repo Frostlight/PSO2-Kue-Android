6b93b80effe16303fd81c53d5b40e506dce03df9
Vincent <cloudy.sonata@gmail.com>
2015-05-27 05:44:43 -0400

    EqContract

--- app/src/main/java/frostlight/pso2kue/data/EqContract.java
+++ app/src/main/java/frostlight/pso2kue/data/EqContract.java
@@ -6,4 +6,49 @@ package frostlight.pso2kue.data;
  * Created by Vincent on 5/19/2015.
  */
 public class EqContract {
+
+    /**
+     * Inner class that defines the table contents of the calendar table
+     * The calendar table stores the emergency quest schedule obtained from Google calendar
+     * Table: ID | EQ Name | Date/Time
+     */
+    public static final class CalendarEntry {
+        public static final String TABLE_NAME = "calendar";
+
+        // The name of the emergency quest
+        public static final String COLUMN_EQNAME = "eq_name";
+
+        // The time the emergency quest occurs
+        public static final String COLUMN_DATE = "date";
+    }
+
+    /**
+     * Inner class that defines the table contents of the Twitter table
+     * The twitter table stores emergency quest alert tweets obtained from Twitter bots
+     * Table: ID | EQ Name | Date/Time
+     */
+    public static final class TwitterEntry {
+        public static final String TABLE_NAME = "twitter";
+
+        // The name of the emergency quest
+        public static final String COLUMN_EQNAME = "eq_name";
+
+        // The time the emergency quest occurs
+        public static final String COLUMN_DATE = "date";
+    }
+
+    /**
+     * Inner class that defines the table contents of the translation table
+     * The translation table stores the japanese/english pairs for the emergency quest names
+     * Table: ID | EQ Name | Date/Time
+     */
+    public static final class TranslationEntry {
+        public static final String TABLE_NAME = "translation";
+
+        // The name of the emergency quest
+        public static final String COLUMN_JAPANESE = "japanese";
+
+        // The time the emergency quest occurs
+        public static final String COLUMN_ENGLISH = "english";
+    }
 }
