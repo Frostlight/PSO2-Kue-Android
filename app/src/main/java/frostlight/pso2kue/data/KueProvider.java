84a7fa131d584b311c85fa5e739f6c6451f11ed4
Vincent <cloudy.sonata@gmail.com>
2015-06-08 17:02:21 -0400

    Content provider functions

--- app/src/main/java/frostlight/pso2kue/data/KueProvider.java
+++ app/src/main/java/frostlight/pso2kue/data/KueProvider.java
@@ -4,6 +4,7 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 /**
@@ -59,27 +60,170 @@ public class KueProvider extends ContentProvider {
             case TRANSLATION:
                 return KueContract.TranslationEntry.CONTENT_TYPE;
             default:
-                throw new UnsupportedOperationException("Unknown uri: " + uri);
+                throw new UnsupportedOperationException("Error: Unknown URI " + uri);
         }
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        return null;
+        // Start by getting a readable database
+        final SQLiteDatabase sqLiteDatabase = mDbHelper.getReadableDatabase();
+        Cursor returnCursor;
+
+        switch(sUriMatcher.match(uri)) {
+            case CALENDAR:
+                returnCursor = sqLiteDatabase.query(
+                        KueContract.CalendarEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            case TWITTER:
+                returnCursor = sqLiteDatabase.query(
+                        KueContract.TwitterEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            case TRANSLATION:
+                returnCursor = sqLiteDatabase.query(
+                        KueContract.TwitterEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            default:
+                throw new UnsupportedOperationException("Error: Unknown URI " + uri);
+        }
+
+        // Set the Uri to observe on the cursor for content observers
+        returnCursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        // Return the cursor containing the results of the query
+        return returnCursor;
     }
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        return null;
+        // Start by getting a writable database
+        final SQLiteDatabase sqLiteDatabase = mDbHelper.getWritableDatabase();
+        Uri returnUri;
+        long _id; // ID of the new database entry
+
+        // Use the uriMatcher to match the URI's we are going to handle
+        // If it doesn't match these, throw an UnsupportedOperationException
+        switch(sUriMatcher.match(uri))
+        {
+            case CALENDAR:
+                _id = sqLiteDatabase.insert(KueContract.CalendarEntry.TABLE_NAME, null, values);
+                if (_id > 0)
+                    returnUri = KueContract.CalendarEntry.buildCalendarUri(_id);
+                else
+                    throw new android.database.SQLException("Error: Failed to insert row into " + uri);
+                break;
+            case TWITTER:
+                _id = sqLiteDatabase.insert(KueContract.TwitterEntry.TABLE_NAME, null, values);
+                if (_id > 0)
+                    returnUri = KueContract.TwitterEntry.buildTwitterUri(_id);
+                else
+                    throw new android.database.SQLException("Error: Failed to insert row into " + uri);
+                break;
+            case TRANSLATION:
+                _id = sqLiteDatabase.insert(KueContract.TranslationEntry.TABLE_NAME, null, values);
+                if (_id > 0)
+                    returnUri = KueContract.TranslationEntry.buildTranslationUri(_id);
+                else
+                    throw new android.database.SQLException("Error: Failed to insert row into " + uri);
+                break;
+            default:
+                throw new UnsupportedOperationException("Error: Unknown URI " + uri);
+        }
+
+        // Notify the content observers
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        // Return the Uri of the entry that was just inserted
+        return returnUri;
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+        // Start by getting a writable database
+        final SQLiteDatabase sqLiteDatabase = mDbHelper.getWritableDatabase();
+        Uri returnUri;
+        int deleteCount; // Number of rows deleted
+
+        // Use the uriMatcher to match the URI's we are going to handle
+        // If it doesn't match these, throw an UnsupportedOperationException
+        switch(sUriMatcher.match(uri))
+        {
+            case CALENDAR:
+                deleteCount = sqLiteDatabase.delete(KueContract.CalendarEntry.TABLE_NAME, selection,
+                        selectionArgs);
+                break;
+            case TWITTER:
+                deleteCount = sqLiteDatabase.delete(KueContract.TwitterEntry.TABLE_NAME, selection,
+                        selectionArgs);
+                break;
+            case TRANSLATION:
+                deleteCount = sqLiteDatabase.delete(KueContract.TranslationEntry.TABLE_NAME, selection,
+                        selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Error: Unknown URI " + uri);
+        }
+
+        // If rows were deleted, notify the content observers
+        if (deleteCount > 0)
+            getContext().getContentResolver().notifyChange(uri, null);
+
+        // Return the number of rows deleted
+        return deleteCount;
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+        // Start by getting a writable database
+        final SQLiteDatabase sqLiteDatabase = mDbHelper.getWritableDatabase();
+        int updateCount = 0;
+
+        // Use the uriMatcher to match the URI's we are going to handle
+        // If it doesn't match these, throw an UnsupportedOperationException
+        switch(sUriMatcher.match(uri))
+        {
+            case CALENDAR:
+                updateCount = sqLiteDatabase.update(KueContract.CalendarEntry.TABLE_NAME, values,
+                        selection, selectionArgs);
+                break;
+            case TWITTER:
+                updateCount = sqLiteDatabase.update(KueContract.TwitterEntry.TABLE_NAME, values,
+                        selection, selectionArgs);
+                break;
+            case TRANSLATION:
+                updateCount = sqLiteDatabase.update(KueContract.TranslationEntry.TABLE_NAME, values,
+                        selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Error: Unknown URI " + uri);
+        }
+
+        // If rows were updated, notify the content observers
+        if (updateCount > 0)
+            getContext().getContentResolver().notifyChange(uri, null);
+
+        // Return the number of rows updated
+        return updateCount;
     }
 }
