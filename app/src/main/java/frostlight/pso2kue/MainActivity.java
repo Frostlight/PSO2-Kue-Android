0323b6783e3ed202b62197d0b5bfdd402985fd20
Vincent <cloudy.sonata@gmail.com>
2015-06-16 17:52:39 -0400

    Preferences (integration pending)

--- app/src/main/java/frostlight/pso2kue/MainActivity.java
+++ app/src/main/java/frostlight/pso2kue/MainActivity.java
@@ -1,5 +1,6 @@
 package frostlight.pso2kue;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -19,23 +20,21 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle action bar item clicks here
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent settings = new Intent(this, SettingsActivity.class);
+                startActivity(settings);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/frostlight/pso2kue/SettingsActivity.java
+++ app/src/main/java/frostlight/pso2kue/SettingsActivity.java
@@ -1,18 +1,35 @@
 package frostlight.pso2kue;
 
+import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+
+import java.util.List;
 
 /**
  * SettingsActivity
  * A PreferenceActivity that presents a list of Application settings
  * Created by Vincent on 5/19/2015.
  */
-public class SettingsActivity extends PreferenceActivity
-        implements Preference.OnPreferenceChangeListener {
-
+public class SettingsActivity extends PreferenceActivity {
     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Display the fragment as the main content
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.preferences);
+        }
     }
 }
