e4b06f8fc363a5385683da5142f24f83fcdcdcd8
Vincent <cloudy.sonata@gmail.com>
2015-07-11 22:01:08 -0400

    Skip Time attack rankings on calendar Update

--- app/src/main/java/frostlight/pso2kue/FetchCalendarTask.java
+++ app/src/main/java/frostlight/pso2kue/FetchCalendarTask.java
@@ -84,7 +84,8 @@ public class FetchCalendarTask extends AsyncTask<Void, Void, Void> {
                      * Result:      Boost Day, Boost Period, Round 10 Start, Round 10 Ends
                      */
                     if (Utility.matchPattern(entry.title,
-                            "(Boost Day)|(Boost Period)|(Round.*Start)|(Round.*Start)|(Round.*Ends)").length() > 0)
+                            "(Boost Day)|(Boost Period)|(Round.*Start)|(Round.*Start)" +
+                                    "|(Round.*Ends)|(Time Attack Ranking)").length() > 0)
                         continue;
 
                     // Insert each element into the database
