1daf9c5116c8ffa771a9b7372b299963174283b1
Vincent <cloudy.sonata@gmail.com>
2015-09-09 20:28:07 -0400

    Separate for iOS

---
+++ android/app/src/main/java/frostlight/pso2kue/ConstKey.java
@@ -0,0 +1,18 @@
+package frostlight.pso2kue;
+
+/**
+ * ConstKey
+ * Authentication keys and secrets for APIs
+ * Updates to this file should be done manually on GitHub because of the Git --assume-unchanged flag
+ * Created by Vincent on 5/25/2015.
+ */
+public class ConstKey {
+
+    // Authentication keys for using Twitter API (read permission only)
+    static final String twitterKey = "TWITTER_KEY";
+    static final String twitterSecret = "TWITTER_SECRET";
+
+    // Authentication keys for using Bing Translate API
+    static final String bingKey = "BING_KEY";
+    static final String bingSecret = "BING_SECRET";
+}
