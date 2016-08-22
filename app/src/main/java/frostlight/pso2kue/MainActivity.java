b1c2593a4370a14b0ca6d5ff611a513a9c71d750
Vincent <cloudy.sonata@gmail.com>
2016-08-21 20:39:06 -0400

    Close #33: Ads implemented

--- app/src/main/java/frostlight/pso2kue/MainActivity.java
+++ app/src/main/java/frostlight/pso2kue/MainActivity.java
@@ -8,6 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 import frostlight.pso2kue.gcm.GcmHelper;
 import frostlight.pso2kue.gcm.GcmRegistrationTask;
 import frostlight.pso2kue.gcm.GcmUnregistrationTask;
@@ -18,11 +22,30 @@ import frostlight.pso2kue.gcm.GcmUnregistrationTask;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private AdView mAdView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        // Initialize the Mobile Ads SDK.
+        MobileAds.initialize(this, "ca-app-pub-8451127810761579~8909900444");
+
+        // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
+        // values/strings.xml.
+        mAdView = (AdView) findViewById(R.id.ad_view);
+
+        // Create an ad request. Check your logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        // Start loading the ad in the background.
+        mAdView.loadAd(adRequest);
 
         // Load the default preferences in case the user hasn't accessed the PreferenceActivity yet
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
@@ -41,6 +64,33 @@ public class MainActivity extends AppCompatActivity {
         // If notifications are off and there is a saved registration ID, unregister with the backend
         else if (!notify && (regId != null && !regId.equals("")))
             new GcmUnregistrationTask(this).execute(regId);
+    }
+
+    /** Called when leaving the activity */
+    @Override
+    public void onPause() {
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+        super.onPause();
+    }
+
+    /** Called when returning to the activity */
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    /** Called before the activity is destroyed */
+    @Override
+    public void onDestroy() {
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+        super.onDestroy();
     }
 
     @Override
